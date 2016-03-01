d067d957c7ed0a8883535f17015f6cd8dd09a9d1
Eric Johnson <edj.boston@gmail.com>
2016-02-29 21:45:22 -0800

    Whitespace in requires

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const coveralls = require('gulp-coveralls'),
-    david     = require('gulp-david'),
-    eslint    = require('gulp-eslint'),
-    gulp      = require('gulp'),
-    istanbul  = require('gulp-istanbul'),
-    mocha     = require('gulp-mocha'),
-    rules     = require('edj-eslint-rules'),
-    sequence  = require('gulp-sequence');
+    david       = require('gulp-david'),
+    eslint      = require('gulp-eslint'),
+    gulp        = require('gulp'),
+    istanbul    = require('gulp-istanbul'),
+    mocha       = require('gulp-mocha'),
+    rules       = require('edj-eslint-rules'),
+    sequence    = require('gulp-sequence');
 
 
 // Instrument the code
